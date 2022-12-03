ffd8da7f9180c1df865b9d296b38222510349a4f
yizhong520 <72602630+yizhong520@users.noreply.github.com>
2022-12-03 13:51:52 -0500

    Update order_resource.py

--- Microservice/COMS6156-sprint1-microservice2/src/order_resource.py
+++ Microservice/COMS6156-sprint1-microservice2/src/order_resource.py
@@ -27,7 +27,7 @@ class OrderResource:
     @staticmethod
     def get_by_key(key):
 
-        sql = "SELECT * FROM coms6156_sprint1_microservice2.order where orderID=%s";
+        sql = "SELECT * FROM coms6156_sprint1_microservice2.order where orderID=%s"
         conn = OrderResource._get_connection()
         cur = conn.cursor()
         res = cur.execute(sql, args=key)
@@ -35,3 +35,38 @@ class OrderResource:
 
         return result
 
+    @staticmethod
+    def add(order):
+        placeholder = ", ".join(["%s"] * len(order))
+        sql = "insert into {table} ({columns}) values ({values});".format(table='coms6156_sprint1_microservice2.order',
+                                                                             columns=",".join(order.keys()),
+                                                                             values=placeholder)
+        print(sql)
+        conn = OrderResource._get_connection()
+        cur = conn.cursor()
+        res =cur.execute(sql, list(order.values()))
+        result = cur.fetchone()
+
+        return res
+
+    def update(order):
+        placeholder = ", ".join(["%s"] * len(order))
+        sql = "update  coms6156_sprint1_microservice2.order set shopID=%s, productID=%s, date=%s, num=%s where orderID=%s"
+        print(sql)
+        conn = OrderResource._get_connection()
+        cur = conn.cursor()
+        res =cur.execute(sql, [order['shopID'],order['productID'],order['date'],order['num'],order['orderID']])
+        result = cur.fetchone()
+
+        return res
+
+    @staticmethod
+    def delete_by_key(key):
+
+        sql = "Delete  FROM coms6156_sprint1_microservice2.order where orderID=%s"
+        conn = OrderResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=key)
+        result = cur.fetchone()
+
+        return res
